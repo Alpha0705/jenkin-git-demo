9d749a04095b6c2b08d76fc1daa87442a59bea15
AlphaArius07 <131933173+Alpha0705@users.noreply.github.com>
2024-10-21 11:53:23 +0530

    Create jenkin-java-pipeline.groovy

---
+++ jenkin-java-pipeline.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Compile Java Program') {
+            steps {
+                // Compile the Java program from the JenkinsPipeline folder
+                bat 'javac C:\\JenkinsPipeline\\Add.java'
+            }
+        }
+        stage('Run Java Program') {
+            steps {
+                // Run the Java program from the JenkinsPipeline folder
+                // -cp = class path: used to execute the class file for the compiled java file
+                bat 'java -cp C:\\JenkinsPipeline Add'
+            }
+        }
+    }
+}
